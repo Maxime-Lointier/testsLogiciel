52192ad9f9653e5b341a4f3eaa8c79ca87fdee31
LOINTIER Maxime <12300457@q10428.iutv.univ-paris13.fr>
2025-05-02 12:01:55 +0200

    test

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -14,4 +14,27 @@ public class VoitureTest {
         assertEquals(1,1);
     }
 
+    @Test
+    void marque(){
+       Voiture voiture = new Voiture("BMW",10);
+        assertEquals("BMW",voiture.getMarque());
+    }
+
+    @Test
+    void Testprix(){
+       Voiture voiture = new Voiture("BMW",10);
+        assertEquals(10,voiture.getPrix());
+    }
+    @Test
+    void TestId(){
+       Voiture voiture = new Voiture("BMW",10);
+        voiture.setId(1);
+        assertEquals(voiture.getId(), 1);
+    }
+    @Test
+    void TestSetPrix(){
+       Voiture voiture = new Voiture("BMW",10);
+        voiture.setPrix(25);
+        assertEquals(25,voiture.getPrix());
+    }
 }
